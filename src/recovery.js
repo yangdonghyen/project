2bc4ee299dc432249bffeb9e7845a13aa3cb3ea4
yangdonghyen <154607772+yangdonghyen@users.noreply.github.com>
2024-04-19 22:38:57 +0900

    Update recovery.js

--- src/recovery.js
+++ src/recovery.js
@@ -1,28 +1,28 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 function recoverFile(filePath, tool, callback) {
-    let command;
+  let command;
 
-    switch (tool) {
-        case 'photorec':
-            command = `photorec ${filePath}`;
-            break;
-        case 'testdisk':
-            command = `testdisk ${filePath}`;
-            break;
-        case 'scalpel':
-            command = `scalpel ${filePath} -o output_directory`;
-            break;
-        default:
-            return callback(new Error('Invalid recovery tool selected'));
+  switch (tool) {
+    case "photorec":
+      command = `"C:\\Users\\ddong\\Desktop\\photorec.exe" ${filePath}`;
+      break;
+    case "testdisk":
+      command = `"C:\\Users\\ddong\\Desktop\\testdisk_win" ${filePath}`;
+      break;
+    case "scalpel":
+      command = `scalpel ${filePath} -o output_directory`;
+      break;
+    default:
+      return callback(new Error("Invalid recovery tool selected"));
+  }
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      return callback(error);
     }
-
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            return callback(error);
-        }
-        callback(null, { stdout, stderr });
-    });
+    callback(null, { stdout, stderr });
+  });
 }
 
 module.exports = { recoverFile };
